68dfeea75e2c536c6095243962c30f5a20daf3d7
JOHNSONI <Ian.Johnson@JRJNET.com>
2019-09-02 20:59:39 -0400

    commiting files after work has been complete

--- Problem1.py
+++ Problem1.py
@@ -1,7 +1,7 @@
 ##This is inefficent way of completing, try to complete when index goes to 1,000,000,000
 ##Need to review Calculus series to get more efficent way of completing
 answer = 0
-for index in range(1000):
+for index in range(1, 1000):
     if index % 3 == 0 or index % 5 == 0:
         answer += index
 print(answer)
