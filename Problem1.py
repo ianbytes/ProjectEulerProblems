e28ff14b13cf20c6cc8f96ea93a89736a85dbce5
JOHNSONI <Ian.Johnson@JRJNET.com>
2019-09-02 20:15:11 -0400

    adding comments to remind myself to that if I knew calculus this would be much more efficent

--- Problem1.py
+++ Problem1.py
@@ -1,5 +1,12 @@
+##This is inefficent way of completing, try to complete when index goes to 1,000,000,000
+##Need to review Calculus series to get more efficent way of completing
 answer = 0
 for index in range(1000):
     if index % 3 == 0 or index % 5 == 0:
         answer += index
 print(answer)
+
+
+
+
+
